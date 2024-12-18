01834872d6d89c19b53923533a436380185682f3
Valen943 <valentinodidio943@gmail.com>
2024-12-18 20:16:56 -0300

    - Bumped sdk and unity version requirements - Added AnimatorToggle and LocalObjectRespawn scripts

---
+++ Runtime/Scripts/AnimatorToggle.cs
@@ -0,0 +1,19 @@
+﻿using UdonSharp;
+using UnityEngine;
+
+namespace valenvrc.Common{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync),Icon("Packages/com.valenvrc.common/Runtime/PromotionalImages/ValenFace.jpg"), HelpURL("https://discord.gg/nv5ax3wDqc")]
+    public class AnimatorToggle : UdonSharpBehaviour
+    {
+        [SerializeField] Animator anim;
+        [SerializeField] string parameterName;
+
+        void OnEnable(){
+            anim.SetBool(parameterName, true);
+        }
+
+        void OnDisable(){
+            anim.SetBool(parameterName, false);
+        }
+    }
+}
---
+++ Runtime/Scripts/LocalObjectRespawn.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UdonSharp;
+
+namespace valenvrc.Common{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync),Icon("Packages/com.valenvrc.common/Runtime/PromotionalImages/ValenFace.jpg"), HelpURL("https://discord.gg/nv5ax3wDqc")]
+    public class LocalObjectRespawn : UdonSharpBehaviour
+    {
+        [SerializeField] GameObject objectToRespawn;
+        [SerializeField] GameObject respawnPoint;
+
+        public override void Interact(){
+            objectToRespawn.transform.position = respawnPoint.transform.position;
+            objectToRespawn.transform.rotation = respawnPoint.transform.rotation;
+        }
+    }
+}
