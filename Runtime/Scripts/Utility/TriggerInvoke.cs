b39aae34bf22f78e0b8c35caa73758258f0c0246
Valen943 <valentinodidio943@gmail.com>
2025-06-26 17:47:03 -0300

    Added TriggerInvoke script

---
+++ Runtime/Scripts/Utility/TriggerInvoke.cs
@@ -0,0 +1,24 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace valenvrc.Common
+{
+    [RequireComponent(typeof(Collider))]
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None), Icon("Packages/com.valenvrc.common/Editor/Resources/ValenFace.jpg"), HelpURL("https://discord.gg/nv5ax3wDqc")]
+    public class TriggerInvoke : UdonSharpBehaviour
+    {
+        [SerializeField] string[] methodNames = new string[1];
+        [SerializeField] UdonBehaviour[] targets = new UdonBehaviour[1];
+
+        public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+        {
+            if (!player.isLocal) return;
+            for (int i = 0; i < targets.Length; i++)
+                if (targets[i] != null && !string.IsNullOrEmpty(methodNames[i]))
+                    targets[i].SendCustomEvent(methodNames[i]);
+        }
+    }
+}
