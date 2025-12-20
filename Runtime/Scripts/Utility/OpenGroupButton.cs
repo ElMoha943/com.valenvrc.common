958746e30a929c1da28aa0342fd2d8ede3301d97
Valentino Casadidio <valentinodidio943@gmail.com>
2025-12-20 17:39:37 -0300

    added vals creation as default group id as example

--- Runtime/Scripts/Utility/OpenGroupButton.cs
+++ Runtime/Scripts/Utility/OpenGroupButton.cs
@@ -6,7 +6,7 @@ namespace valenvrc.Common{
     [UdonBehaviourSyncMode(BehaviourSyncMode.None), HelpURL("https://discord.gg/nv5ax3wDqc"), Icon("Packages/com.valenvrc.common/Editor/Resources/ValenFace.jpg")]
     public class OpenGroupButton : UdonSharpBehaviour{
 
-        [SerializeField] string GroupIdentifier;
+        [SerializeField] string GroupIdentifier = "grp_32fd99ba-2ec3-4408-9dce-74cf4ea0b713";
 
         public override void Interact(){
             _OpenGroupPage();
